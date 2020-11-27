5a90ae1e1a6c5b83c817caf3c520738f959978af
Dipankar Pal <dipankarpal5050@gmail.com>
2020-11-27 11:24:14 +0000

    :gear: ability to comment

--- dist/index.js
+++ dist/index.js
@@ -10,17 +10,36 @@ const core = __webpack_require__(2186);
 const github = __webpack_require__(5438);
 const Axios = __webpack_require__(6545);
 
-console.log("workflow started....");
-
-const github_token = core.getInput('GITHUB_TOKEN',{required: true});
 
 
-const context = github.context;
 
-const author = context.payload.sender.login;
-const repoOwner = context.payload.repository.owner.login;
+const comment_report = async (context, issue_number, message) => {
+    const author = context.payload.sender.login;
 
-console.log(context);
+    const octokit = github.getOctokit(github_token);
+    const comment = await octokit.issues.createComment({
+        issue_number: issue_number,
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        body: message
+    });
+}
+
+const run = () => {
+    console.log("workflow started....");
+
+    const github_token = core.getInput('GITHUB_TOKEN', { required: true });
+    const issue_number = core.getInput('issue_number', { required: true });
+    const options = core.getInput('options', { required: true });
+
+    const context = github.context;
+    console.log(context);
+
+    comment_report(context, issue_number, "hi there :tada:");
+
+}
+
+run();
 
 /***/ }),
 
--- index.js
+++ index.js
@@ -3,14 +3,33 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const Axios = require('axios');
 
-console.log("workflow started....");
-
-const github_token = core.getInput('GITHUB_TOKEN',{required: true});
 
 
-const context = github.context;
 
-const author = context.payload.sender.login;
-const repoOwner = context.payload.repository.owner.login;
+const comment_report = async (context, issue_number, message) => {
+    const author = context.payload.sender.login;
 
-console.log(context);
+    const octokit = github.getOctokit(github_token);
+    const comment = await octokit.issues.createComment({
+        issue_number: issue_number,
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        body: message
+    });
+}
+
+const run = () => {
+    console.log("workflow started....");
+
+    const github_token = core.getInput('GITHUB_TOKEN', { required: true });
+    const issue_number = core.getInput('issue_number', { required: true });
+    const options = core.getInput('options', { required: true });
+
+    const context = github.context;
+    console.log(context);
+
+    comment_report(context, issue_number, "hi there :tada:");
+
+}
+
+run();
