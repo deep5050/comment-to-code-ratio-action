04bb4aa738be0facbd6869a11b89e8ee75fe947a
Dipankar Pal <dipankarpal5050@gmail.com>
2020-11-27 11:48:03 +0000

    :gear: cloc added

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
-const Axios = require('axios');
+const { exec } = require("child_process");
 
 
+const run_command = async (options) => {
+    const command = `cloc ${options}`
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
 
-
-const comment_report = async (context,github_token, issue_number, message) => {
+const comment_report = async (context, github_token, issue_number, message) => {
     const author = context.payload.sender.login;
 
     const octokit = github.getOctokit(github_token);
@@ -28,7 +41,12 @@ const run = () => {
     const context = github.context;
     console.log(context);
 
-    comment_report(context, github_token,issue_number, "hi there :tada:");
+    run_command(options).then((data) => {
+        comment_report(context, github_token, issue_number, "hi there :tada:");
+    }).catch((err) => {
+        console.log(err);
+    })
+
 
 }
 
