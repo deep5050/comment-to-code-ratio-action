e9170996435998206327a86d60d752e8b38e2769
Dipankar Pal <dipankarpal5050@gmail.com>
2020-11-28 15:10:17 +0000

    :bug: fix

--- index.js
+++ index.js
@@ -1,61 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { exec } = require("child_process");
+const exace = require('execa');
 const fs = require('fs');
 
 
-const run_command = async (options) => {
+const run_command = (options) => {
     console.log("Running command");
-    const command = `cloc ${options}`
-    await exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            throw error;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            throw stderr;
-        }
-        // console.log(`stdout: ${stdout}`);
-        console.log("commad succeed");
-        return "";
-    });
+    try {
+        var options_array = options.split(' ');
+        execa.sync('cloc', options_array);
+        execa
+    } catch (error) {
+        throw new Error(`Faild to execute the command: ${{ error }}`);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const comment_report = async (context, github_token, issue_number, message) => {
@@ -73,23 +31,20 @@ const comment_report = async (context, github_token, issue_number, message) => {
 }
 
 
-const run = async() => {
+const run = async () => {
     const github_token = core.getInput('GITHUB_TOKEN', { required: true });
     const issue_number = core.getInput('issue_number', { required: true });
     const options = core.getInput('options', { required: true });
-
     const context = github.context;
-    
-    await run_command(options);
 
-    fs.readFile('./report.md','utf8',(err,data)=>{
-        if (err) throw err;
-        if(data)
-        {
-            console.log(data)
-        comment_report(context,github_token,issue_number,data);
-        }
-    });
+    try {
+        run_command(options);
+        const report_text = fs.readFileSync('report.md', 'utf-8');
+        await comment_report(context, github_token, issue_number, report_text);
+    } catch (error) {
+        throw new Error(`Failed to read the file: ${{ error }}`)
+    }
+
 }
 
 run();
